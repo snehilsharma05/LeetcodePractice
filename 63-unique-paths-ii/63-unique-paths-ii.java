189278a9e06254e3fcaad5c73bd93a49b2d2868a
snehilsharma05 <44290942+snehilsharma05@users.noreply.github.com>
2022-05-20 10:47:34 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (81.50%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,12 +1,32 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length, n = obstacleGrid[0].length;
-        int[][] dp = new int[m][n];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        int[][] dp = new int[m+1][n+1];
+        dp[m-1][n-1] = 1;
+        
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(obstacleGrid[i][j] == 1){
+                    dp[i][j] = 0;
+                }else if(i== m-1 && j == n-1){
+                    dp[i][j] = 1;
+                }else{
+                    int right = 0;
+                    int down = 0;
+                    if(j<n-1){
+                        right += dp[i][j+1];
+                    }
+                    if(i<m-1){
+                        down += dp[i+1][j];
+                    }
+                    dp[i][j] = right + down;
+                }
+            }
         }
-        return unique(obstacleGrid,0,0,dp);
+        return dp[0][0];
+        //return unique(obstacleGrid,0,0,dp);
     }
+    
     private int unique(int[][] grid,int m,int n,int[][] dp){
         if(m>=grid.length || n>=grid[0].length || grid[m][n] == 1){
             return 0;
