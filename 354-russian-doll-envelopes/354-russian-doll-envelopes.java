6135d31c7a148515cf910efecf671f7af3a90f59
snehilsharma05 <44290942+snehilsharma05@users.noreply.github.com>
2022-05-25 18:59:28 +0530

    Time: 84 ms (32.01%), Space: 80.2 MB (64.63%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.java
@@ -1,11 +1,11 @@
 class Solution {
     public int maxEnvelopes(int[][] E) {
         Arrays.sort(E, (a,b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
-        int[] dp = new int[E.length];
+        int[] dp = new int[E.length]; //dp will be maintained by us sorted
         int ans = 0;
         for (int[] env : E) {
             int height = env[1];
-            int left = Arrays.binarySearch(dp, 0, ans, height);
+            int left = Arrays.binarySearch(dp, 0, ans, height);//in given array, from [0...index], find the value height
             if (left < 0) left = -left - 1;
             if (left == ans) ans++;
             dp[left] = height;
