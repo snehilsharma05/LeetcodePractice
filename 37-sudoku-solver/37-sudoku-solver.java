0fd9c6b000f0ca9e993d09fc0b40fab394f319e1
snehilsharma05 <44290942+snehilsharma05@users.noreply.github.com>
2022-05-28 17:05:36 +0530

    Time: 21 ms (35.22%), Space: 41.8 MB (45.47%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+    
+    private boolean solve(char[][] board){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j] == '.'){
+                    for(char ch = '1';ch<='9';ch++){
+                        if(isSafe(board,i,j,ch)){
+                            board[i][j] = ch;
+                            if(solve(board)){
+                                return true;
+                            }else{
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    private boolean isSafe(char[][] board,int row,int col,char ch){
+        for(int i=0;i<9;i++){
+            if(board[i][col] == ch){
+                return false;
+            }
+            
+            if(board[row][i] == ch){
+                return false;
+            }
+            
+            if(board[3*(row/3) + i/3][3*(col/3) + i % 3] == ch){
+                return false;
+            }
+        }
+        return true;
+    }
+}
