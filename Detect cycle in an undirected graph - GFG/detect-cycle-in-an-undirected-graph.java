f73ad2d330f5d11f4bb77e132a204277f64d6cbb
snehilsharma05 <44290942+snehilsharma05@users.noreply.github.com>
2022-05-31 21:08:29 +0530

    Added solution - LeetHub

---
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isCycle(V, adj);
+            if (ans)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution {
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int N, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        boolean[] visited = new boolean[N];
+        for(int v=0;v<N;v++){
+            if(!visited[v]){
+                if(bfs(v,adj,visited)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    private boolean bfs(int node,ArrayList<ArrayList<Integer>> adj,boolean[] visited){
+        
+        //remove mark* w add*
+        Queue<Integer> q = new LinkedList<>();
+        q.add(node);
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            if(visited[curr]){
+                return true;
+            }
+            
+            visited[curr] = true;
+            for(int nbr:adj.get(curr)){
+                if(!visited[nbr]){
+                    q.add(nbr);
+                }
+            }
+        }
+        return false;
+    }
+}
