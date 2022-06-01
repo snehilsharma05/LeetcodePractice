f39cef3ca58b65b512263e5d00139b163f283309
snehilsharma05 <44290942+snehilsharma05@users.noreply.github.com>
2022-06-01 22:47:26 +0530

    Time: 4 ms (61.82%), Space: 43.2 MB (35.28%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,12 +1,21 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
-        int[][] dp = new int[n][2*k+1];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
-        }
+        int[][] dp = new int[n+1][2*k+1];
         
-        return maxProfit(0,0,prices,k,dp);
+        for(int index = n-1;index>=0;index--){
+            for(int transNum = 2*k-1;transNum>=0;transNum--){
+                if(transNum % 2 == 0){
+                     dp[index][transNum] =  Math.max(-prices[index] + dp[index+1][transNum+1],
+                            0 + dp[index+1][transNum]);
+                }else{
+                     dp[index][transNum] = Math.max(+prices[index] + dp[index+1][transNum+1],
+                                    0 + dp[index+1][transNum]);
+                }
+            }
+        }
+        return dp[0][0];
+        //return maxProfit(0,0,prices,k,dp);
     }
     private int maxProfit(int index,int transNum,int[] prices,int k,int[][] dp){
         if(index >= prices.length || transNum == 2*k){
