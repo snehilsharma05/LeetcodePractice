80a5556c86800d2d4e7439446a7e147120727a33
snehilsharma05 <44290942+snehilsharma05@users.noreply.github.com>
2022-06-05 09:23:30 +0530

    Time: 1 ms (97.89%), Space: 41 MB (51.20%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.java
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -1,59 +1,30 @@
 class Solution {
+    private int count = 0;
     public int totalNQueens(int n) {
-         List<List<String>> ans = new ArrayList<>();
-        char[][] board = new char[n][n];
-        for(char[] row:board){
-            Arrays.fill(row,'.');
-        }
+        count = 0;
+        boolean[] row = new boolean[n];
+        boolean[] leftTop = new boolean[2*n+1];
+        boolean[] leftBottom = new boolean[2*n+1];
         
-        solve(0,board,ans);
-        return ans.size();
+        backTrack(0,n,row,leftTop,leftBottom);
+        return count;
     }
-     private void solve(int col,char[][] board,List<List<String>> ans){
-        if(col == board.length){
-            List<String> copyBoard = new ArrayList<>();
-            for(char[] row:board){
-                String str = new String(row);
-                copyBoard.add(str);
-            }
-            ans.add(copyBoard);
+    private void backTrack(int currCol,int n, boolean[] row,boolean[] leftTop, boolean[] leftBottom){
+        if(currCol == n){
+            count++;
             return;
         }
         
-        for(int row=0;row<board.length;row++){
-            if(isPossible(board,row,col)){
-                board[row][col] = 'Q';
-                solve(col+1,board,ans);
-                board[row][col] = '.';
+        for(int r=0;r<n;r++){
+            int index1 = r + currCol;
+            int index2 = currCol - r + n;
+            if(row[r] || leftTop[index1] || leftBottom[index2]){
+                continue;
             }
+            
+            row[r] =  leftTop[index1] = leftBottom[index2] = true;
+            backTrack(currCol+1,n,row,leftTop,leftBottom);
+            row[r] =  leftTop[index1] = leftBottom[index2] = false;
         }
-    }
-    private boolean isPossible(char[][] board,int row,int col){
-        int rowDup = row,colDup = col;
-        
-        while(row>=0 && col>=0){
-            if(board[row][col] == 'Q'){
-                return false;
-            }    
-            row--;
-            col--;
-        }
-        
-        row = rowDup; col = colDup;
-        while(row<board.length && col>=0){
-            if(board[row][col] == 'Q'){
-                return false;
-            }
-            row++;
-            col--;
-        }
-        row = rowDup; col = colDup;
-        while(col>=0){
-            if(board[row][col] == 'Q'){
-                return false;
-            }
-            col--;
-        }
-        return true;
     }
 }
