cbc35a656d4564956003ffe0a6611115b7c14198
snehilsharma05 <44290942+snehilsharma05@users.noreply.github.com>
2022-06-06 09:51:35 +0530

    Time: 1 ms (53.16%), Space: 42.4 MB (40.76%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,12 +1,28 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
         int[][] dp = new int[m][n];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
-        }
         
-        return ways(m-1,n-1,obstacleGrid,dp);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == 1){
+                    dp[i][j] = 0;
+                }else if(i == 0 && j == 0){
+                    dp[i][j] = 1;
+                }else{
+                    int left = 0,up = 0;
+                    if(i>0){
+                        up += dp[i-1][j];
+                    }
+                    if(j>0){
+                        left += dp[i][j-1];
+                    }
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+        //return ways(m-1,n-1,obstacleGrid,dp);
     }
     private int ways(int m,int n,int[][] grid,int[][] dp){
         if(m<0 || n<0 || grid[m][n] == 1){
