38d498d92bf5c249b908457e25b208f4dc1a17a4
snehilsharma05 <44290942+snehilsharma05@users.noreply.github.com>
2022-06-06 09:45:38 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (40.76%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,47 +1,25 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length, n = obstacleGrid[0].length;
-        int[][] dp = new int[m+1][n+1];
-        dp[m-1][n-1] = 1;
-        
-        for(int i=m-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                if(obstacleGrid[i][j] == 1){
-                    dp[i][j] = 0;
-                }else if(i== m-1 && j == n-1){
-                    dp[i][j] = 1;
-                }else{
-                    int right = 0;
-                    int down = 0;
-                    if(j<n-1){
-                        right += dp[i][j+1];
-                    }
-                    if(i<m-1){
-                        down += dp[i+1][j];
-                    }
-                    dp[i][j] = right + down;
-                }
-            }
+        int[][] dp = new int[m][n];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
         }
-        return dp[0][0];
-        //return unique(obstacleGrid,0,0,dp);
+        
+        return ways(m-1,n-1,obstacleGrid,dp);
     }
-    
-    private int unique(int[][] grid,int m,int n,int[][] dp){
-        if(m>=grid.length || n>=grid[0].length || grid[m][n] == 1){
+    private int ways(int m,int n,int[][] grid,int[][] dp){
+        if(m<0 || n<0 || grid[m][n] == 1){
             return 0;
         }
         
-        if(m == grid.length-1 && n == grid[0].length-1){
+        if(m == 0 && n == 0){
             return 1;
         }
-        
         if(dp[m][n] != -1){
             return dp[m][n];
         }
         
-        int right = unique(grid,m,n+1,dp);
-        int down = unique(grid,m+1,n,dp);
-        return  dp[m][n] = right + down;
+        return dp[m][n] = ways(m-1,n,grid,dp) + ways(m,n-1,grid,dp);
     }
 }
