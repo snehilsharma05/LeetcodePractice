b6c79f1fb3e545fdf07075b356e20b1b29f7e9bf
snehilsharma05 <44290942+snehilsharma05@users.noreply.github.com>
2022-06-13 14:51:32 +0530

    Time: 33 ms (63.14%), Space: 54.3 MB (17.20%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean isMatch(String s, String p) {
         int m = s.length(), n = p.length();
-        int[][] dp = new int[m][n];
+        int[][] dp = new int[m+1][n+1];
         for(int[] row:dp){
             Arrays.fill(row,-1);
         }
         
-        return isMatch(m-1,n-1,s,p,dp);
+        return isMatch(m,n,s,p,dp);
     }
     private boolean isMatch(int m,int n,String s,String p,int[][] dp){
-        if(m == -1 && n == -1){
+        if(m == 0 && n == 0){
             return true;
-        }else if(n<0){
+        }else if(n == 0){
             return false;
-        }else if(m<0){
-            for(int i=0;i<=n;i++){
+        }else if(m == 0){
+            for(int i=0;i<n;i++){
                 if(p.charAt(i) != '*'){
                     return false;
                 }
@@ -25,10 +25,10 @@ class Solution {
             return dp[m][n] == 1;
         }
         
-        if(s.charAt(m) == p.charAt(n) || p.charAt(n) == '?'){
+        if(s.charAt(m-1) == p.charAt(n-1) || p.charAt(n-1) == '?'){
             dp[m][n] = isMatch(m-1,n-1,s,p,dp)==true?1:0;
         }else{
-            if(p.charAt(n) == '*'){
+            if(p.charAt(n-1) == '*'){
                  dp[m][n] = (isMatch(m-1,n,s,p,dp) || isMatch(m,n-1,s,p,dp)) == true? 1 : 0;
             }else{
                 dp[m][n] = 0;
