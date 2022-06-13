eb8b4480742144c2be3bfb8bb22abd81a69c0c72
snehilsharma05 <44290942+snehilsharma05@users.noreply.github.com>
2022-06-13 15:08:49 +0530

    Time: 77 ms (13.47%), Space: 47.7 MB (58.01%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -1,12 +1,41 @@
 class Solution {
     public boolean isMatch(String s, String p) {
         int m = s.length(), n = p.length();
-        int[][] dp = new int[m+1][n+1];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        boolean[][] dp = new boolean[m+1][n+1];
+        
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = true;
+                }else if(j == 0){
+                    dp[i][j] = false;
+                }else if(i == 0){
+                    boolean flag = true;
+                    for(int trav=1;trav<=j;trav++){
+                        if(p.charAt(trav-1) != '*'){
+                            flag =  false;
+                            break;
+                        }
+                    }
+                    dp[i][j] = flag;
+                } else if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    if(p.charAt(j-1) == '*'){
+                         dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                    }else{
+                        dp[i][j] = false;
+                    }
+                }
+            }
         }
         
-        return isMatch(m,n,s,p,dp);
+        // int[][] dp = new int[m+1][n+1];
+        // for(int[] row:dp){
+        //     Arrays.fill(row,-1);
+        // }
+        return dp[m][n];
+        //return isMatch(m,n,s,p,dp);
     }
     private boolean isMatch(int m,int n,String s,String p,int[][] dp){
         if(m == 0 && n == 0){
@@ -14,8 +43,8 @@ class Solution {
         }else if(n == 0){
             return false;
         }else if(m == 0){
-            for(int i=0;i<n;i++){
-                if(p.charAt(i) != '*'){
+            for(int trav=1;trav<=n;trav++){
+                if(p.charAt(trav-1) != '*'){
                     return false;
                 }
             }
