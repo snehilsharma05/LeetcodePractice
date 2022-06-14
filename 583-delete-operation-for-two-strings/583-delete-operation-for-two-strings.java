9cff84b8fea0e53ad9ebe19337ef8a13a411ff4d
snehilsharma05 <44290942+snehilsharma05@users.noreply.github.com>
2022-06-14 12:30:40 +0530

    Time: 14 ms (51.72%), Space: 46.6 MB (63.12%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
@@ -1,23 +1,14 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int m = word1.length(), n = word2.length();
-        int[] prev = new int[n+1];
-        
-        for(int i=1;i<=m;i++){
-            int[] curr = new int[n+1];
-            for(int j=1;j<=n;j++){
-                if(word1.charAt(i-1) == word2.charAt(j-1)){
-                    curr[j] = 1 + prev[j-1];
-                }else{
-                    curr[j] = Math.max(curr[j-1],prev[j]);
-                }
-            }
-            prev = curr;
+        int m = word1.length(),n = word2.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
         }
         
-        return m + n - 2 * prev[n];
+        return m + n - 2* LCS(m,n,word1,word2,dp);
     }
-    private int LCS(String word1,String word2,int m,int n,int[][] dp){
+    private int LCS(int m,int n,String word1,String word2,int[][] dp){
         if(m == 0 || n == 0){
             return 0;
         }
@@ -27,9 +18,9 @@ class Solution {
         }
         
         if(word1.charAt(m-1) == word2.charAt(n-1)){
-            return dp[m][n] = 1 + LCS(word1,word2,m-1,n-1,dp);
+            return dp[m][n] = 1 + LCS(m-1,n-1,word1,word2,dp);
         }else{
-            return dp[m][n] = Math.max(LCS(word1,word2,m-1,n,dp), LCS(word1,word2,m,n-1,dp));
+            return dp[m][n] = Math.max(LCS(m-1,n,word1,word2,dp),LCS(m,n-1,word1,word2,dp));
         }
     }
 }
