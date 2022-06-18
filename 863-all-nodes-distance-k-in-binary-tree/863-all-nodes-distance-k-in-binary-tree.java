26ebc4861ec842e12311afc455009f5c0e74fe7c
snehilsharma05 <44290942+snehilsharma05@users.noreply.github.com>
2022-06-18 16:48:22 +0530

    Time: 16 ms (52.84%), Space: 43.6 MB (20.37%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
@@ -13,38 +13,38 @@ class Solution {
             return new ArrayList<>();
         }
         
-        Map<Integer,TreeNode> parents = makeParents(root);
+        Map<TreeNode,TreeNode> parents = makeParents(root);
         return nodesWithDistanceK(target,parents,k);
         
     }
     
-    private Map<Integer,TreeNode> makeParents(TreeNode root){
-        Map<Integer,TreeNode> parents = new HashMap<>();
+    private Map<TreeNode,TreeNode> makeParents(TreeNode root){
+        Map<TreeNode,TreeNode> parents = new HashMap<>();
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
         
         while(!q.isEmpty()){
             TreeNode curr = q.poll();
             if(curr.left != null){
-                parents.put(curr.left.val, curr);
+                parents.put(curr.left, curr);
                 q.add(curr.left);
             }
             
             if(curr.right != null){
-                parents.put(curr.right.val,curr);
+                parents.put(curr.right,curr);
                 q.add(curr.right);
             }
         }
         return parents;
     }
     
-    private List<Integer> nodesWithDistanceK(TreeNode target, Map<Integer,TreeNode> parents, int k){
+    private List<Integer> nodesWithDistanceK(TreeNode target, Map<TreeNode,TreeNode> parents, int k){
         int currDist = 0;
         Queue<TreeNode> q = new LinkedList<>();
-        Set<Integer> visited = new HashSet<>();
+        Set<TreeNode> visited = new HashSet<>();
         
         q.add(target);
-        visited.add(target.val);
+        visited.add(target);
         
         while(!q.isEmpty()){
             
@@ -57,19 +57,19 @@ class Solution {
             
             while(n-->0){
                 TreeNode curr = q.poll();
-                if(curr.left != null && !visited.contains(curr.left.val)){
+                if(curr.left != null && !visited.contains(curr.left)){
                     q.add(curr.left);
-                    visited.add(curr.left.val);
+                    visited.add(curr.left);
                 }
                 
-                if(curr.right != null && !visited.contains(curr.right.val)){
+                if(curr.right != null && !visited.contains(curr.right)){
                     q.add(curr.right);
-                    visited.add(curr.right.val);
+                    visited.add(curr.right);
                 }
                 
-                if(parents.containsKey(curr.val) && !visited.contains(parents.get(curr.val).val)){
-                    q.add(parents.get(curr.val));
-                    visited.add(parents.get(curr.val).val);
+                if(parents.containsKey(curr) && !visited.contains(parents.get(curr))){
+                    q.add(parents.get(curr));
+                    visited.add(parents.get(curr));
                 }
             }
         }
