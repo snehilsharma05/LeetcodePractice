b8a2347de8859686f4249d04930c6f91e08e423c
snehilsharma05 <44290942+snehilsharma05@users.noreply.github.com>
2022-06-24 21:58:24 +0530

    Time: 5 ms (75.29%), Space: 50.8 MB (68.22%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -13,36 +13,62 @@
  *     }
  * }
  */
+class BSTIterator{
+    private Stack<TreeNode> s;
+    private boolean reverse;
+    
+    BSTIterator(TreeNode root,boolean reverse){
+        this.reverse = reverse;
+        s = new Stack<>();
+        pushAll(root);
+    }
+    
+    public boolean hasNext(){
+        return !s.isEmpty();
+    }
+    
+    public int next(){
+            TreeNode curr = s.pop();
+            if(reverse){
+                pushAll(curr.left);
+            }else{
+                pushAll(curr.right);
+            }
+            
+            return curr.val;
+       
+    }
+    
+    private void pushAll(TreeNode root){
+        while(root != null){
+            s.push(root);
+            if(reverse){
+                root = root.right;
+            }else{
+                root = root.left;
+            }
+        }
+    }
+}
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
         if(root == null){
             return false;
         }
         
-        List<Integer> in = new ArrayList<>();
-        storeInorder(root,in);
+        BSTIterator start = new BSTIterator(root,false);
+        BSTIterator end = new BSTIterator(root,true);
         
-        int low = 0,high = in.size()-1;
-        while(low<high){
-            int sum = in.get(low) + in.get(high);
-            if(sum == k){
+        int val1 = start.next(),val2 = end.next();
+        while(val1<val2){
+            if(val1 + val2 == k){
                 return true;
-            }
-            if(sum > k){
-                high--;
+            }else if(val1 + val2 > k){
+                val2 = end.next();
             }else{
-                low++;
+                val1 = start.next();
             }
         }
         return false;
-    }
-    private void storeInorder(TreeNode root,List<Integer> in){
-        if(root == null){
-            return;
-        }
-        
-        storeInorder(root.left,in);
-        in.add(root.val);
-        storeInorder(root.right,in);
     }
 }
