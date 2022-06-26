007058b4332dc1f0d27ecba5303f397ae641fa7e
snehilsharma05 <44290942+snehilsharma05@users.noreply.github.com>
2022-06-26 18:37:59 +0530

    Time: 4 ms (27.36%), Space: 42.1 MB (49.01%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            
+            if(ch =='(' || ch=='{' || ch == '['){
+                stack.push(ch);
+            }else if(ch ==')'){
+                if(stack.isEmpty() || stack.pop()!='('){
+                    return false;
+                }
+            }else if(ch == '}'){
+                if(stack.isEmpty() || stack.pop()!='{'){
+                    return false;
+                }
+            }else if(ch == ']'){
+                if(stack.isEmpty() || stack.pop()!='['){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
