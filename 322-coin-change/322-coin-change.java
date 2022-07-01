98a79b635e798bbb20924bd026cb4c5c06878c69
snehilsharma05 <44290942+snehilsharma05@users.noreply.github.com>
2022-07-01 21:58:42 +0530

    Time: 68 ms (15.99%), Space: 55 MB (19.14%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,48 +1,40 @@
 class Solution {
-    public int coinChange(int[] coins, int totalAmount) {
+    public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int[][] dp = new int[n][totalAmount+1];
         
-        for(int amount=0;amount<=totalAmount;amount++){
-            if(amount % coins[0] == 0){
-                dp[0][amount] = amount/coins[0];
-            }else{
-                dp[0][amount] = (int)1e9;
-            }
+        int[][] dp = new int[n][amount+1];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
         }
         
-        for(int index=1;index<n;index++){
-            for(int amount=0;amount<=totalAmount;amount++){
-                int notTake = 0 + dp[index-1][amount];
-                int take = (int)1e9;
-                if(coins[index]<=amount){
-                    take = 1 + dp[index][amount-coins[index]];
-                }
-                dp[index][amount] = Math.min(take,notTake);
-            }
+        int ans = minCoins(n-1,amount,coins,dp);
+        if(ans >= (int)1e9){
+            return -1;
         }
-        return dp[n-1][totalAmount] == (int)1e9 ? -1 : dp[n-1][totalAmount];
-        
-        //int ans = minCoins(n-1,coins,amount,dp);
-        
+        return ans;
     }
-    private int minCoins(int index,int[] coins,int amount,int[][] dp){
-        if(index == 0){
-            if(amount % coins[index] == 0){
-                return amount/coins[index];
-            }else{
-                return (int)1e9;
-            }
+    
+    private int minCoins(int index,int amount,int[] coins,int[][] dp){
+        if(amount == 0){
+            return 0;
         }
         
         if(dp[index][amount] != -1){
             return dp[index][amount];
         }
         
-        int notTake = 0 + minCoins(index-1,coins,amount,dp);
-        int take = (int)1e9;
-        if(amount>=coins[index]){
-            take = 1 + minCoins(index,coins,amount - coins[index],dp);
+        if(index == 0){
+            if(amount%coins[index] == 0){
+                return amount/coins[index];
+            }else{
+                return (int)1e9;
+            }
+        }
+        
+        int notTake = minCoins(index-1,amount,coins,dp);
+        int take = (int)(1e9);
+        if(coins[index]<=amount){
+            take = 1 + minCoins(index,amount - coins[index],coins,dp);
         }
         return dp[index][amount] = Math.min(take,notTake);
     }
