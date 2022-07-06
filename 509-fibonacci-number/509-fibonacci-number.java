95200b8dbcab81ccfc29d82d0d14a75ed89ca934
snehilsharma05 <44290942+snehilsharma05@users.noreply.github.com>
2022-07-06 10:28:06 +0530

    Time: 0 ms (100.00%), Space: 41 MB (34.36%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int fib(int n) {
+        if(n<=1){
+            return n;
+        }
+        
+        int prev2 = 0,prev = 1;
+        for(int i=2;i<=n;i++){
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+}
