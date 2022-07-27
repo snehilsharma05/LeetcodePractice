a94abec506e858ce129b22a37b556dc951f497a1
snehilsharma05 <44290942+snehilsharma05@users.noreply.github.com>
2022-07-27 22:26:04 +0530

    Time: 1 ms (59.83%), Space: 44.7 MB (34.31%) - LeetHub

--- 598-range-addition-ii/598-range-addition-ii.java
+++ 598-range-addition-ii/598-range-addition-ii.java
@@ -1,11 +1,7 @@
 class Solution {
     public int maxCount(int m, int n, int[][] ops) {
-        int max = ops.length;
-        if(max == 0){
-            return m*n;
-        }
         
-        int rowMin = Integer.MAX_VALUE,colMin = Integer.MAX_VALUE;
+        int rowMin = m,colMin = n;
         for(int[] op:ops){
             rowMin = Math.min(rowMin,op[0]);
             colMin = Math.min(colMin,op[1]);
