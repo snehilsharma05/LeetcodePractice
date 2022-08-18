e601b6aee671970a88678fc5f981a22d289979aa
snehilsharma05 <44290942+snehilsharma05@users.noreply.github.com>
2022-08-18 14:18:48 +0530

    Time: 89 ms (58.26%), Space: 93.1 MB (33.37%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,35 +1,29 @@
 class Solution {
     public int minSetSize(int[] arr) {
         int n = arr.length;
-        
-        //store freq of each num
         Map<Integer,Integer> map = new HashMap<>();
-        for(int num:arr){
-            map.put(num,map.getOrDefault(num,0)+1);
+        for(int i=0;i<n;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
         }
         
-        //count how many numbers are there with given frequency 'i'
-        int[] freq = new int[n+1]; //index 'i' will be the frequency
-        
-        for(int num:map.keySet()){
-            int count = map.get(num);
-            freq[count]++; //we got a number with freq = count
+        int[] freq = new int[n+1];
+        for(int key:map.keySet()){
+            int count = map.get(key);
+            freq[count]++;
         }
         
-        int minSetElements = 0;
-        int stepsTaken = 0;
+        int elementsReqd = 0;
+        int steps = 0;
         for(int i=n;i>0;i--){
-            
             while(freq[i]>0){
-                stepsTaken += i;
-                minSetElements++;
-                freq[i]--; //we used one element stored in freq array
-                if(stepsTaken >= n/2){
-                    return minSetElements;
+                steps += i;
+                elementsReqd++;
+                freq[i]--;
+                if(steps>=n/2){
+                    return elementsReqd;
                 }
             }
-            
         }
-        return minSetElements;
+        return elementsReqd;
     }
 }
