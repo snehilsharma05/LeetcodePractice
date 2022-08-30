234092705e7837708a201afcc4e00f4b1c6a4e98
snehilsharma05 <44290942+snehilsharma05@users.noreply.github.com>
2022-08-30 14:23:10 +0530

    Time: 1 ms (51.92%), Space: 42.2 MB (60.21%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -20,27 +20,17 @@ class Solution {
             return ans;
         }
         
-        Stack<TreeNode> s1 = new Stack<>();
-        Stack<Integer> s2 = new Stack<>();
-        
-        s1.push(root);
-        while(!s1.isEmpty()){
-            TreeNode curr = s1.pop();
-  
-            if(curr.left != null){
-                s1.push(curr.left);
-            }
-            
-            if(curr.right != null){
-                s1.push(curr.right);
-            }
-            
-            s2.push(curr.val);
-        }
-        
-        while(!s2.isEmpty()){
-            ans.add(s2.pop());
-        }
+        postorder(root,ans);
         return ans;
-     }
+    }
+    
+    private void postorder(TreeNode root,List<Integer> ans){
+        if(root == null){
+            return;
+        }
+        
+        postorder(root.left,ans);
+        postorder(root.right,ans);
+        ans.add(root.val);
+    }
 }
